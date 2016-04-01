40268bcedbf8a7594747f6addd934745b579d729
jbw0033 <jbw0033@auburn.edu>
2016-03-31 19:33:10 -0500

    Files for the final turn in

---
+++ Networks Turn In/packet.cpp
@@ -0,0 +1,107 @@
+/*
+ * packet.cpp
+ *
+ *  Created on: Mar 4, 2016
+ *      Author: jeremy
+ */
+
+#include "packet.h"
+#include <sstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void setSeqNum(int sn, Packet * packet) {
+	packet->seqNum = sn;
+}
+
+void setCkSum(int ck, Packet * packet) {
+	packet->ckSum = ck;
+}
+
+void setAck(int ak, Packet * packet) {
+	packet->ack = ak;
+}
+
+bool getSeqNum(Packet packet)  {
+	return packet.seqNum;
+}
+
+int getCkSum(Packet packet) {
+	return packet.ckSum;
+}
+
+int getAck(Packet packet) {
+	return packet.ack;
+}
+
+bool ckSum(Packet packet) {
+	return packet.ckSum == generateCkSum(packet);
+}
+
+int generateCkSum(Packet packet) {
+	int cs = 0;
+	if(packet.data == NULL) {
+		return -1;
+	}
+
+	for (int i = 0; i < sizeof(packet.data); i++) {
+		if (packet.data[i] == '\0') {
+			i == sizeof(packet.data);
+			break;
+		}
+		cs +=  packet.data[i];
+	}
+	if(cs <= 9999) {
+		cs *= 10;
+	}
+
+	if(cs > 0)	{
+		return cs;
+	}
+	return -1;
+
+}
+
+void loadData(char* data, Packet* packet) {
+	strcpy(packet->data, data);
+}
+
+char*  getData(Packet packet) {
+	return packet.data;
+}
+
+void createPacket(int sn, const char data[249], Packet * packet) {
+	packet->seqNum = (sn + 1) % 2;
+	strcpy(packet->data, data);
+	packet->ckSum = generateCkSum(*packet);
+	packet->ack = 0;
+}
+
+char* str(Packet packet) {
+	std::string tempStr(packet.data);
+	std::string packetString;
+	std::string csStr;
+	
+	std::stringstream stream;
+	stream << (long long int) packet.ckSum;
+	csStr = stream.str();
+
+	while (csStr.length() < 5) csStr += '0';
+
+	std::stringstream snhold;
+	
+	snhold << (long long int) packet.seqNum;
+
+	std::stringstream ackhold;
+	ackhold << (long long int) packet.ack;
+
+
+	packetString = snhold.str() + csStr + ackhold.str() + tempStr;
+	strcpy(packet.packet, packetString.c_str());
+	
+	return packet.packet;
+}
+
+
